26b63f05b27e21a0cc78f870e94fa26a923cde01
renderbox <renderbox@gmail.com>
2011-08-21 11:25:52 -0700

    added fields to model forms

--- audiohack/player/models.py
+++ audiohack/player/models.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 class TrackForm(ModelForm):
     class Meta:
         model = Track
+        fields = ("url", "title")
 
 class AnnotationForm(ModelForm):
     class Meta:
         model = Annotation
+        fields = ("start", "end", "type", "url", "description")
