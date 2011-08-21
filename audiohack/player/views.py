68ea2832423b6eb6bc6d917184f2b5fd3696ae39
Esther N <esthernam@gmail.com>
2011-08-21 14:22:52 -0700

    Filling out basic views

--- audiohack/player/views.py
+++ audiohack/player/views.py
@@ -2,7 +2,7 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from audiohack.player.models import Track, Annotation
+from player.models import Track, Annotation, TrackForm, AnnotationForm
 
 def player(request):
     '''
@@ -15,23 +15,30 @@ def player(request):
 def all_tracks(request):
     '''
     Display all tracks
-    ''''
-    pass
+    '''
+    tracks = Track.objects.all(order_by='-added_timestamp')[:9]
+    ctx = RequestContext(request, {'tracks':tracks})
+    return render_to_response('tracks.html', ctx)
 
-def track(request):
+def track(request, track):
     '''
     Single track page
     '''
-    pass
+    track = Track.objects.get(id=track)
+    annotations = Annotation.objects.filter(track_id=track)
+    ctx = RequestContext(request, {'track':track, 'annotations':annotations})
+    return render_to_response('track.html', ctx)
     
 def annotate(request, track):
     '''
     Add annotation
     '''
-    pass
+    ctx = RequestContext(request, {})    
+    return render_to_response('player.html', ctx )
+    
 
 def add_track(request):
     '''
     Add a track to the audio cloud
     '''
-    
+    pass
