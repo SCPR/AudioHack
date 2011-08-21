298ba156ff19953468099b2161a88bdb86dadc94
renderbox <renderbox@gmail.com>
2011-08-21 15:33:42 -0700

    Fixed admin error

--- audiohack/urls.py
+++ audiohack/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import *
+from django.contrib import admin
+admin.autodiscover()
+
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
