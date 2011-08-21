3aea452e1bd1dde2698e1970856077e6f6deaa49
Esther N <esthernam@gmail.com>
2011-08-21 13:33:36 -0700

    Adding skeleton views

--- audiohack/player/views.py
+++ audiohack/player/views.py
@@ -1,6 +1,8 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from audiohack.player.models import Track, Annotation
 
 def player(request):
     '''
@@ -9,3 +11,27 @@ def player(request):
     ctx = RequestContext(request, {})
     
     return render_to_response('player.html', ctx )
+
+def all_tracks(request):
+    '''
+    Display all tracks
+    ''''
+    pass
+
+def track(request):
+    '''
+    Single track page
+    '''
+    pass
+    
+def annotate(request, track):
+    '''
+    Add annotation
+    '''
+    pass
+
+def add_track(request):
+    '''
+    Add a track to the audio cloud
+    '''
+    
