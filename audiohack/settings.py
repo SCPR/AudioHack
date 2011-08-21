ab4d73f813aafc733775b1849ad0ade28aa1018c
Katharine <kjarmul@gmail.com>
2011-08-21 10:56:18 -0700

    Adding app directory

--- audiohack/settings.py
+++ audiohack/settings.py
@@ -89,6 +89,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'player',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
