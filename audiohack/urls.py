a7cc13f749981fe49df8b4df88a9655171a8edb5
renderbox <renderbox@gmail.com>
2011-08-21 15:40:17 -0700

    uncommented Admin tools

--- audiohack/urls.py
+++ audiohack/urls.py
@@ -15,8 +15,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
+    (r'^admin/', include(admin.site.urls)),
 )
