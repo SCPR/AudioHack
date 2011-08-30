deda889c53db438354f619c75457c268c8f485ae
renderbox <renderbox@gmail.com>
2011-08-30 10:22:19 -0700

    Added static file serving and template tags for the player

--- audiohack/settings.py
+++ audiohack/settings.py
@@ -4,6 +4,9 @@ import os.path
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+# Global variable for where the app is run from regardless of machine.
+PROJECT_OS_DIRECTORY = os.path.dirname(__file__)
+
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
 )
@@ -28,7 +31,7 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Los_Angeles'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -78,11 +81,16 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'audiohack.urls'
 
+# Static File Settings
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ( os.path.join( PROJECT_OS_DIRECTORY, 'templates/static' ), )
+
+# Template Directories
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join( os.path.dirname(__file__), 'templates' ),
+    os.path.join( PROJECT_OS_DIRECTORY, 'templates' ),
 )
 
 INSTALLED_APPS = (
@@ -91,6 +99,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'player',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
